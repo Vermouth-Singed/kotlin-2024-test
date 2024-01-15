38fcca5266f4f72ab3d893f18fe489eba8b5148b
Vermouth <qhddusl2004@naver.com>
2024-01-15 23:25:18 +0900

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "kotlin-2024-test"
---
+++ src/test/kotlin/com/example/sample/Kotlin2024TestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.sample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Kotlin2024TestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
